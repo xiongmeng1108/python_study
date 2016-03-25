e97b495ed311e1c4dd6631d690067327ee8bfe4b
xiongmeng1108 <cs_xm@126.com>
2016-03-25 11:29:50 +0800

    Update property.py
    修改property.py

--- advanced/property.py
+++ advanced/property.py
@@ -1,2 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+'example of using @property'
+
+__author__ = 'xiongmeng'
+
+
+class Student(object):
+    # --------------------------------
+    # 1.read and write property : birth
+    @property
+    def birth(self):
+        return self._birth
+
+    @birth.setter
+    def birth(self, value):
+        if not isinstance(value, (int, float)):
+            raise ValueError('birth must be a int or float!')
+        if value < 0 or value > 2016:
+            raise ValueError('birth must be 0<=birth<=2016 !')
+        self._birth = value
+
+    # --------------------------------
+    # 2.readonly property : age
+    @property
+    def age(self):
+        return 2016 - self._birth
+
+
+def test():
+    s = Student()
+    s.birth = 2011
+    print 'birth=%d, age=%d' % (s.birth, s.age)
+
+
+if __name__ == '__main__':
+    test()
